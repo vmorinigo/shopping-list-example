27592174fe5f98c75c53460993e7dd036524210b
Valeria Morinigo <vmorinigo@users.campus>
2020-02-24 15:35:29 -0600

    In class work

---
+++ ShoppingList/AddToShoppingList.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddToShoppingList.xaml
+    /// </summary>
+    public partial class AddToShoppingList : UserControl
+    {
+        public AddToShoppingList()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+           if (DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+
+
+        }
+    }
+}
